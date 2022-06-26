17cd664b7e94ccc477c9957b33a702ed5b2e9a7f
imriSh <59433290+imriShCodeArt@users.noreply.github.com>
2022-06-26 08:45:41 +0300

    CRA to webpack config
    installed webpack, created aliases for easy components importing

--- router-and-redux-tutorial/src/App.js
+++ router-and-redux-tutorial/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import Theme from './theme'
 
 import { Route, Routes } from 'react-router-dom'
---
+++ router-and-redux-tutorial/webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+const deps = require('./package.json').dependencies
+module.exports = {
+  output: {
+    publicPath: 'http://localhost:3000/',
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
+    alias: {
+      components: path.resolve(__dirname, './src/components'),
+      cards: path.resolve(__dirname, './src/cards'),
+      pages: path.resolve(__dirname, './src/pages'),
+      theme: path.resolve(__dirname, './src/theme'),
+    },
+  },
+
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+  ],
+}
