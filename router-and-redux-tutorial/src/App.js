39bec91115be7c29d4a314cb874760ac75101a4b
imriSh <59433290+imriShCodeArt@users.noreply.github.com>
2022-07-06 07:24:33 +0300

    temporary update
    the Theme component is currently doing the routing for the app

--- router-and-redux-tutorial/src/App.js
+++ router-and-redux-tutorial/src/App.js
@@ -1,27 +1,9 @@
 import React from 'react'
+
 import Theme from './theme'
 
-import { Route, Routes } from 'react-router-dom'
-import Home from 'pages/Home'
-import Page from 'pages/Page'
-import About from 'pages/About'
-import Contact from 'pages/Contact'
-
 function App() {
-  return (
-    <Theme>
-      <Routes>
-        <Route path='/*'>
-          <Route index element={<Home />} />
-          <Route path={'about'} element={<About />} />
-          <Route path={'contact'} element={<Contact />} />
-        </Route>
-        <Route path='entry'>
-          <Route path=':posts' element={<>ELM</>} />
-        </Route>
-      </Routes>
-    </Theme>
-  )
+  return <Theme />
 }
 
 export default App
